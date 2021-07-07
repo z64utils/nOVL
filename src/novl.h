62a6bc510d55893df9594f54b20cab54368924b4
z64me <20zz.org@gmail.com>
2021-07-07 02:40:51 -0400

    update version number

--- src/novl.h
+++ src/novl.h
@@ -40,11 +40,11 @@
 
 #define NOVL_VERSION_MAJOR  1
 #define NOVL_VERSION_MINOR  0
-#define NOVL_VERSION_MICRO  2
-#define NOVL_VERSION_STR    "v1.0.2"
+#define NOVL_VERSION_MICRO  3
+#define NOVL_VERSION_STR    "v1.0.3"
 #define NOVL_NAME           "nOVL"
 #define NOVL_FULL_NAME      "Nintendo Overlay Tool"
-#define NOVL_AUTHOR         "Marshall R. <mbr@64.vg> and fixes by z64me <z64.me>"
+#define NOVL_AUTHOR         "Marshall R. <mbr@64.vg> with fixes by Sauraen & z64me <z64.me>"
 
 /* Relocation return values */
 #define NOVL_RELOC_FAIL     2
